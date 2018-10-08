93a67619d7acad6e09b4c0fe40f6dd0c172e0c7e
wennier <fiete.wennier@adesso.de>
2018-10-08 13:33:19 +0200

    Empty accordion to build assignment page added

--- src/app/build-assignment/build-assignment.module.ts
+++ src/app/build-assignment/build-assignment.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BuildAssignmentComponent } from './build-assignment.component';
 import { BuildAssignmentRoutingModule } from './build-assignment-routing.module';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
 
 @NgModule({
   imports: [
     CommonModule,
-    BuildAssignmentRoutingModule
+    BuildAssignmentRoutingModule,
+    AccordionModule
   ],
   declarations: [BuildAssignmentComponent]
 })
