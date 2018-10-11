fc55a6eb4789510465f0166c861daa9a389972cb
fi3te <fi3te@users.noreply.github.com>
2018-10-11 22:22:41 +0200

    Universal service implemented

--- src/app/service/http/universal.service.ts
+++ src/app/service/http/universal.service.ts
@@ -1,15 +1,35 @@
-import { REST_UNIVERSAL } from './common/constants';
-import { DTO } from './../../generated-dtos.model';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import PrincipalDTO = DTO.PrincipalDTO;
-import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
+import { DTO } from './../../generated-dtos.model';
+import { REST_UNIVERSAL } from './common/constants';
+
+import PrincipalDTO = DTO.PrincipalDTO;
+import DashboardInformationDTO = DTO.DashboardInformationDTO;
 
 @Injectable({
   providedIn: 'root'
 })
 export class UniversalService {
   constructor(private httpClient: HttpClient) {}
+
+  public scenarios(): Observable<any> {
+    return this.httpClient.get(`${REST_UNIVERSAL}/scenarios`);
+  }
+
+  public roles(): Observable<any> {
+    return this.httpClient.get(`${REST_UNIVERSAL}/roles`);
+  }
+
+  public getDashboardInformation(
+    teamId: number
+  ): Observable<DashboardInformationDTO> {
+    const params = new HttpParams().set('teamId', `${teamId}`);
+    return this.httpClient.get(`${REST_UNIVERSAL}/dashboard`, {
+      params: params
+    });
+  }
 
   public getPrincipal(): Observable<PrincipalDTO> {
     return this.httpClient.get(`${REST_UNIVERSAL}/principal`);
