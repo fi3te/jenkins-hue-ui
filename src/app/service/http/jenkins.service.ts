7d0f5287cac7cd0271f204d9e1e7c5dedccfe159
fi3te <fi3te@users.noreply.github.com>
2018-10-12 19:07:39 +0200

    Jenkins service implemented

--- src/app/service/http/jenkins.service.ts
+++ src/app/service/http/jenkins.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { DTO } from '../../generated-dtos.model';
+import { REST_JENKINS } from './common/constants';
+
+import JenkinsJobNamesDTO = DTO.JenkinsJobNamesDTO;
 
 @Injectable({
   providedIn: 'root'
 })
 export class JenkinsService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  public getJenkinsUrl(): Observable<string> {
+    return this.httpClient.get<string>(`${REST_JENKINS}/url`);
+  }
+
+  public getJenkinsJobNames(): Observable<JenkinsJobNamesDTO> {
+    return this.httpClient.get(`${REST_JENKINS}/jobs`);
+  }
 }
