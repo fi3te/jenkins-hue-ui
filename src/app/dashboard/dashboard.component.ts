99cbb37dc91a41331e25e4a35fac5a71a46e9cd2
fi3te <fi3te@users.noreply.github.com>
2018-10-12 22:38:26 +0200

    Dashboard connected to back-end

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UniversalService } from '../service/http/universal.service';
+import { SessionService } from '../service/session.service';
+import { DTO } from './../generated-dtos.model';
+import { AlertService } from './../service/alert.service';
+
+import DashboardInformationDTO = DTO.DashboardInformationDTO;
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  public dashboard: DashboardInformationDTO;
 
-  constructor() { }
+  constructor(
+    private sessionService: SessionService,
+    private universalService: UniversalService,
+    private alertService: AlertService
+  ) {}
 
-  ngOnInit() {
+  public ngOnInit(): void {
+    this.universalService
+      .getDashboardInformation(this.sessionService.getTeamId())
+      .subscribe(
+        next => {
+          this.dashboard = next;
+        },
+        error => {
+          this.alertService.danger(error.message);
+        }
+      );
   }
-
 }
