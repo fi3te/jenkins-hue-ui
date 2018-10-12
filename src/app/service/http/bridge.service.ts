e16f7054afa38a45138ecb2eb2abf96141df9f1f
fi3te <fi3te@users.noreply.github.com>
2018-10-12 19:14:52 +0200

    Bridge service implemented

--- src/app/service/http/bridge.service.ts
+++ src/app/service/http/bridge.service.ts
@@ -1,9 +1,52 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { DTO } from '../../generated-dtos.model';
+import { REST_BRIDGES } from './common/constants';
+
+import FoundBridgeDTO = DTO.FoundBridgeDTO;
+import BridgeDTO = DTO.BridgeDTO;
+import BridgeCreateDTO = DTO.BridgeCreateDTO;
+import BridgeUpdateDTO = DTO.BridgeUpdateDTO;
 
 @Injectable({
   providedIn: 'root'
 })
 export class BridgeService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  public findAvailable(): Observable<FoundBridgeDTO[]> {
+    return this.httpClient.get<FoundBridgeDTO[]>(`${REST_BRIDGES}/available`);
+  }
+
+  public find(
+    page: number,
+    size: number,
+    searchItem?: string
+  ): Observable<BridgeDTO[]> {
+    const options = searchItem
+      ? { params: new HttpParams().set('searchItem', `${searchItem}`) }
+      : undefined;
+    return this.httpClient.get<BridgeDTO[]>(`${REST_BRIDGES}`);
+  }
+
+  public count(searchItem?: string): Observable<number> {
+    const options = searchItem
+      ? { params: new HttpParams().set('searchItem', `${searchItem}`) }
+      : undefined;
+    return this.httpClient.get<number>(`${REST_BRIDGES}/count`, options);
+  }
+
+  public create(bridgeCreateDTO: BridgeCreateDTO): Observable<BridgeDTO> {
+    return this.httpClient.post(`${REST_BRIDGES}/create`, bridgeCreateDTO);
+  }
+
+  public update(bridgeUpdateDTO: BridgeUpdateDTO): Observable<BridgeDTO> {
+    return this.httpClient.post(`${REST_BRIDGES}/update`, bridgeUpdateDTO);
+  }
+
+  public remove(id: number): Observable<any> {
+    return this.httpClient.delete(`${REST_BRIDGES}/remove/${id}`);
+  }
 }
