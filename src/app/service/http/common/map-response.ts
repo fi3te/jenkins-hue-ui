eb659c759729ee4b5664b2d324c1a161999e7e56
wennier <fiete.wennier@adesso.de>
2018-10-12 19:16:21 +0200

    Unnecessary response mapping removed

--- src/app/service/http/common/map-response.ts
+++
@@ -1,5 +0,0 @@
-import { Observable, OperatorFunction } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { Response } from '@angular/http';
-
-export default (() => map((response: Response) => response.json()))();
