ae5e91bcbf16774e491a07e8346bfbb23dee34e0
wennier <fiete.wennier@adesso.de>
2018-10-18 18:53:38 +0200

    Remove button component added

---
+++ src/app/build-assignment/remove-button/remove-button.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-remove-button',
+  templateUrl: './remove-button.component.html',
+  styleUrls: ['./remove-button.component.scss']
+})
+export class RemoveButtonComponent {}
