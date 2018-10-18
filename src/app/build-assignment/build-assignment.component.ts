9edbaf66bdca41e885dcd6d3f2606ce8bd68fb3f
wennier <fiete.wennier@adesso.de>
2018-10-18 20:32:34 +0200

    AngularJS code replaced

--- src/app/build-assignment/build-assignment.component.ts
+++ src/app/build-assignment/build-assignment.component.ts
@@ -1,10 +1,15 @@
-import { AddScenarioModalComponent } from './add-scenario-modal/add-scenario-modal.component';
-import { AddJobsModalComponent } from './add-jobs-modal/add-jobs-modal.component';
 import { Component, OnInit } from '@angular/core';
-import { DTO } from '../generated-dtos.model';
-import LampDTO = DTO.LampDTO;
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+
+import { DTO } from '../generated-dtos.model';
+import { SessionService } from '../service/session.service';
+import { LampService } from './../service/http/lamp.service';
+import { AddJobsModalComponent } from './add-jobs-modal/add-jobs-modal.component';
+import { AddScenarioModalComponent } from './add-scenario-modal/add-scenario-modal.component';
+
+import TeamLampsDTO_LampDTO = DTO.TeamLampsDTO_LampDTO;
+import Scenario = DTO.Scenario;
 
 @Component({
   selector: 'app-build-assignment',
@@ -13,20 +18,37 @@ import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 })
 export class BuildAssignmentComponent implements OnInit {
 
-  public lampDTOs: LampDTO[];
+  public lampDTOs: TeamLampsDTO_LampDTO[];
+  public scenarioPriority: Scenario[];
 
-  constructor(private modalService: BsModalService) { }
+  public test = new Date();
+
+  constructor(
+    private modalService: BsModalService,
+    private sessionService: SessionService,
+    private lampService: LampService
+  ) {}
 
   public ngOnInit(): void {
+    const teamId = this.sessionService.getTeamId();
+    this.lampService.findAllOfATeam(teamId).subscribe(next => {
+      this.lampDTOs = next.lamps;
+      this.scenarioPriority = next.scenarioPriority;
+      this.fix();
+      this.sortScenarioConfigs();
+    });
+  }
+
+  public addJobsDemo(): void {
     const initialState = {
       jobNames: []
     };
 
-    const bsModalRef: BsModalRef = this.modalService.show(AddJobsModalComponent, {initialState});
-
+    const bsModalRef: BsModalRef = this.modalService.show(
+      AddJobsModalComponent,
+      { initialState }
+    );
     // this.bsModalRef.content.closeBtsnName = 'Close';
-
-    this.addScenariosDemo();
   }
 
   public addScenariosDemo(): void {
@@ -34,6 +56,40 @@ export class BuildAssignmentComponent implements OnInit {
       scenarios: []
     };
 
-    const bsModalRef: BsModalRef = this.modalService.show(AddScenarioModalComponent, {initialState});
+    const bsModalRef: BsModalRef = this.modalService.show(
+      AddScenarioModalComponent,
+      { initialState }
+    );
+  }
+
+  // TODO remove
+  private fix(): void {
+    for (const lamp of this.lampDTOs) {
+      lamp.workingStart = new Date(lamp.workingStart);
+      lamp.workingEnd = new Date(lamp.workingEnd);
+    }
+  }
+
+  private sortScenarioConfigs(): void {
+    for (const lamp of this.lampDTOs) {
+      if (lamp.buildingConfigs) {
+        lamp.buildingConfigs.sort(this.compareScenarioConfigs);
+      }
+      if (lamp.failureConfigs) {
+        lamp.failureConfigs.sort(this.compareScenarioConfigs);
+      }
+      if (lamp.unstableConfigs) {
+        lamp.unstableConfigs.sort(this.compareScenarioConfigs);
+      }
+      if (lamp.successConfigs) {
+        lamp.successConfigs.sort(this.compareScenarioConfigs);
+      }
+    }
+  }
+
+  private compareScenarioConfigs(a, b): number {
+    const posA = this.scenarioPriority.indexOf(a.scenario);
+    const posB = this.scenarioPriority.indexOf(b.scenario);
+    return posA - posB;
   }
 }
