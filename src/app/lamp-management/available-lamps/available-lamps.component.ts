8b3c6317154a292262f51d66fbfd0bbbb37c3d9b
fi3te <fi3te@users.noreply.github.com>
2018-10-26 09:56:25 +0200

    Old layout copied

--- src/app/lamp-management/available-lamps/available-lamps.component.ts
+++ src/app/lamp-management/available-lamps/available-lamps.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DTO } from '../../generated-dtos.model';
+import LampHueDTO = DTO.LampHueDTO;
+
 @Component({
   selector: 'app-available-lamps',
   templateUrl: './available-lamps.component.html',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AvailableLampsComponent implements OnInit {
 
+  public availableLamps: LampHueDTO[];
+  public isRefreshing: boolean;
+  public isTaking: boolean;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public refresh(): void {
+    // TODO
+  }
+
+  public pulseOnce(lamp: LampHueDTO): void {
+    // TODO
+  }
+
+  public takeLamp(lamp: LampHueDTO): void {
+    // TODO
   }
 
 }
