5a9fec7f30dc623aaeb3f9dbef2b437bf049f3e0
wennier <fiete.wennier@adesso.de>
2018-11-09 21:34:02 +0100

    Bridge list component implemented

--- src/app/bridge-management/bridge-list/bridge-list.component.ts
+++ src/app/bridge-management/bridge-list/bridge-list.component.ts
@@ -1,6 +1,14 @@
-import { BridgeOwnershipService } from './../bridge-ownership.service';
 import { Component, OnInit } from '@angular/core';
 import { delay } from 'rxjs/operators';
+
+import { DTO } from '../../generated-dtos.model';
+import { PagingService } from '../../service/http/paging.service';
+import { SessionService } from '../../service/session.service';
+import { BridgeService } from './../../service/http/bridge.service';
+import { REST_BRIDGES } from './../../service/http/common/constants';
+import { BridgeOwnershipService } from './../bridge-ownership.service';
+
+import BridgeDTO = DTO.BridgeDTO;
 
 @Component({
   selector: 'app-bridge-list',
@@ -8,14 +16,35 @@ import { delay } from 'rxjs/operators';
   styleUrls: ['./bridge-list.component.scss']
 })
 export class BridgeListComponent implements OnInit {
+  public bridgeData: PagingService<BridgeDTO>;
+  public userId: number;
+  public isAdmin: boolean;
 
-  constructor(private bridgeOwnershipService: BridgeOwnershipService) { }
-
-  public ngOnInit(): void {
-    // delay for the HueSDK to connect to the bridge (is needed for a set hue user property)
-    this.bridgeOwnershipService.bridgeCreated$.pipe(delay(400)).subscribe(() => {
-      // TODO
-    });
+  constructor(
+    private bridgeOwnershipService: BridgeOwnershipService,
+    pagingService: PagingService<BridgeDTO>,
+    private sessionService: SessionService,
+    private bridgeService: BridgeService
+  ) {
+    pagingService.setUrl(REST_BRIDGES);
+    this.bridgeData = pagingService;
   }
 
+  public ngOnInit(): void {
+    this.userId = this.sessionService.getUserId();
+    this.isAdmin = this.sessionService.isAdmin();
+    this.sessionService.isAdmin();
+    // delay for the HueSDK to connect to the bridge (is needed for a set hue user property)
+    this.bridgeOwnershipService.bridgeCreated$
+      .pipe(delay(400))
+      .subscribe(() => {
+        this.bridgeData.refresh();
+      });
+  }
+
+  public removeBridge(bridge: BridgeDTO): void {
+    this.bridgeService.remove(bridge.id).subscribe(() => {
+      this.bridgeData.refresh();
+    });
+  }
 }
--- src/app/bridge-management/bridge-management.component.ts
+++ src/app/bridge-management/bridge-management.component.ts
@@ -1,29 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { BsModalService } from 'ngx-bootstrap/modal';
-import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
-import { FoundBridgesModalComponent } from './found-bridges-modal/found-bridges-modal.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-bridge-management',
   templateUrl: './bridge-management.component.html',
   styleUrls: ['./bridge-management.component.scss']
 })
-export class BridgeManagementComponent implements OnInit {
-
-  constructor(private modalService: BsModalService) { }
-
-  public ngOnInit(): void {
-  }
-
-  public openModal(): void {
-    const initialState = {
-      bridges: [],
-      title: 'Modal with component'
-    };
-    const bsModalRef: BsModalRef = this.modalService.show(FoundBridgesModalComponent, {initialState});
-
-    // this.bsModalRef.content.closeBtsnName = 'Close';
-
-  }
-
-}
+export class BridgeManagementComponent {}
