027fb8abc7adf3c8d2c4cde189144ce419c2f920
fi3te <fi3te@users.noreply.github.com>
2018-11-27 21:46:24 +0100

    Unnecessary code removed

--- src/app/user-management/user-management.component.ts
+++ src/app/user-management/user-management.component.ts
@@ -1,24 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from '../service/http/user.service';
-import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
-import { BsModalService } from 'ngx-bootstrap/modal';
-import { ChangeRolesModalComponent } from '../shared/change-roles-modal/change-roles-modal.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-user-management',
   templateUrl: './user-management.component.html',
   styleUrls: ['./user-management.component.scss']
 })
-export class UserManagementComponent implements OnInit {
-
-  constructor(private userService: UserService, private modalService: BsModalService) { }
-
-  public ngOnInit(): void {
-    const initialState = {
-      roles: []
-    };
-
-    const bsModalRef: BsModalRef = this.modalService.show(ChangeRolesModalComponent, {initialState});
-  }
-
-}
+export class UserManagementComponent {}
