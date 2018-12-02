75c0504528a95394549de19e58811cd2179a37d6
wennier <fiete.wennier@adesso.de>
2018-12-02 14:55:15 +0100

    Add team component visibility check added

--- src/app/team-management/team-management.component.ts
+++ src/app/team-management/team-management.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
 
+import { SessionService } from '../service/session.service';
+
 @Component({
   selector: 'app-team-management',
   templateUrl: './team-management.component.html',
   styleUrls: ['./team-management.component.scss']
 })
-export class TeamManagementComponent {}
+export class TeamManagementComponent {
+  public isAdmin: boolean;
+
+  constructor(sessionService: SessionService) {
+    this.isAdmin = sessionService.isAdmin();
+  }
+}
