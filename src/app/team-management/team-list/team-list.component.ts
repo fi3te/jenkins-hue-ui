4f8c2871c70556b6e258b48bbce1702f0b33a25b
fi3te <fi3te@users.noreply.github.com>
2018-12-02 14:54:41 +0100

    Team list component html added

--- src/app/team-management/team-list/team-list.component.ts
+++ src/app/team-management/team-list/team-list.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DTO } from '../../generated-dtos.model';
+
+import TeamUsersDTO = DTO.TeamUsersDTO;
+import { PagingService } from '../../service/http/paging.service';
+import { ActivatedRoute } from '@angular/router';
+import { SessionService } from '../../service/session.service';
+import { SimpleEnum } from '../../service/model/simple-enum.model';
+
 @Component({
   selector: 'app-team-list',
   templateUrl: './team-list.component.html',
@@ -7,9 +15,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamListComponent implements OnInit {
 
-  constructor() { }
+  public teamData: PagingService<TeamUsersDTO>;
+  public isAdmin: boolean;
+  public loadingRoles: boolean;
 
-  ngOnInit() {
+  constructor(
+    private sessionService: SessionService,
+    private route: ActivatedRoute
+  ) { }
+
+  public ngOnInit(): void {
+    this.isAdmin = this.sessionService.isAdmin();
+    this.route.data.subscribe((data: { pagingService: PagingService<TeamUsersDTO> }) => {
+      this.teamData = data.pagingService;
+    });
   }
 
+  public createUser(teamId: number): void {
+    // TODO
+  }
+
+  public changeRoles(userId: number, roles: SimpleEnum[]): void {
+    // TODO
+  }
+
+  public removingUserPossible(userId: number): boolean {
+    // TODO
+    return false;
+  }
+
+  public removeUser(userId: number): void {
+    // TODO
+  }
+
+  public removingTeamPossible(userId: number): boolean {
+    // TODO
+    return false;
+  }
+
+  public removeTeam(teamId: number): void {
+    // TODO
+  }
 }
