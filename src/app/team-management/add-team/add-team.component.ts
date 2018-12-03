ff2ffba3e2bcf26d6b5210e268d7d292dbdd77fd
fi3te <fi3te@users.noreply.github.com>
2018-12-03 21:29:01 +0100

    Add team component implemented

--- src/app/team-management/add-team/add-team.component.ts
+++ src/app/team-management/add-team/add-team.component.ts
@@ -1,21 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { AlertService } from './../../service/alert.service';
+import { TeamService } from './../../service/http/team.service';
+import { TeamManagementService } from './../team-management.service';
+import { Component, ViewChild } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-add-team',
   templateUrl: './add-team.component.html',
   styleUrls: ['./add-team.component.scss']
 })
-export class AddTeamComponent implements OnInit {
+export class AddTeamComponent {
 
   public name: string;
 
-  constructor() { }
+  @ViewChild('teamName')
+  public teamName: NgModel;
 
-  ngOnInit() {
-  }
+  constructor(
+    private teamService: TeamService,
+    private teamManagementService: TeamManagementService,
+    private alertService: AlertService
+  ) { }
 
   public createTeam(): void {
-    // TODO
+    this.teamService.create({name: this.name}).subscribe(() => {
+      this.teamManagementService.teamCreated();
+      this.alertService.info('Team hinzugefügt!');
+    });
+    this.name = '';
+    this.teamName.control.markAsPristine({onlySelf: true});
   }
-
 }
