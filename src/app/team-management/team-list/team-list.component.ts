8711710e985a9a1c98deb3d4a69049c188a9425f
fi3te <fi3te@users.noreply.github.com>
2018-12-05 20:28:41 +0100

    Team list component implemented

--- src/app/team-management/team-list/team-list.component.ts
+++ src/app/team-management/team-list/team-list.component.ts
@@ -1,3 +1,4 @@
+import { TeamManagementService } from './../team-management.service';
 import { Component, OnInit } from '@angular/core';
 
 import { DTO } from '../../generated-dtos.model';
@@ -7,6 +8,13 @@ import { PagingService } from '../../service/http/paging.service';
 import { ActivatedRoute } from '@angular/router';
 import { SessionService } from '../../service/session.service';
 import { SimpleEnum } from '../../service/model/simple-enum.model';
+import { TeamService } from '../../service/http/team.service';
+import { AlertService } from '../../service/alert.service';
+import { RoleService } from '../../shared/role.service';
+import { UserService } from '../../service/http/user.service';
+import { RenameModalComponent } from '../../shared/rename-modal/rename-modal.component';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-team-list',
@@ -19,41 +27,85 @@ export class TeamListComponent implements OnInit {
   public isAdmin: boolean;
   public loadingRoles: boolean;
 
+  private teamId: number;
+  private userId: number;
+
   constructor(
     private sessionService: SessionService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private teamManagementService: TeamManagementService,
+    private modalService: BsModalService,
+    private teamService: TeamService,
+    private userService: UserService,
+    private roleService: RoleService,
+    private alertService: AlertService
   ) { }
 
   public ngOnInit(): void {
     this.isAdmin = this.sessionService.isAdmin();
+    this.teamId = this.sessionService.getTeamId();
+    this.userId = this.sessionService.getUserId();
+
     this.route.data.subscribe((data: { pagingService: PagingService<TeamUsersDTO> }) => {
       this.teamData = data.pagingService;
+    });
+
+    this.teamManagementService.teamCreated$.subscribe(() => {
+      this.teamData.refresh();
     });
   }
 
   public createUser(teamId: number): void {
-    // TODO
+    const initialState = {
+      title: 'Benutzer hinzufügen',
+      icon: 'cui-user'
+    };
+
+    const bsModalRef: BsModalRef = this.modalService.show(
+      RenameModalComponent,
+      { initialState }
+    );
+
+    const subscription = this.modalService.onHide.subscribe(() => {
+      const saved = bsModalRef.content.saved;
+      const name = bsModalRef.content.name;
+      if (saved && name) {
+        this.userService.create({login: name, teamId}).subscribe(() => {
+          this.teamData.refresh();
+          this.alertService.info('Benutzer hinzugefügt!');
+        });
+      } else if (saved) {
+        this.alertService.warning('Eingabe ungültig!');
+      }
+      subscription.unsubscribe();
+    });
   }
 
-  public changeRoles(userId: number, roles: SimpleEnum[]): void {
-    // TODO
+  public changeRoles(userId: number, currentRoles: SimpleEnum[]): void {
+    this.roleService.changeRoles(userId, currentRoles, (loading: boolean) => {
+      this.loadingRoles = loading;
+    }, () => {
+      this.teamData.refresh();
+    });
   }
 
   public removingUserPossible(userId: number): boolean {
-    // TODO
-    return false;
+    return this.userId !== userId;
   }
 
   public removeUser(userId: number): void {
-    // TODO
+    this.userService.remove(userId).subscribe(() => {
+      this.teamData.refresh();
+    });
   }
 
-  public removingTeamPossible(userId: number): boolean {
-    // TODO
-    return false;
+  public removingTeamPossible(teamId: number): boolean {
+    return this.teamId !== teamId;
   }
 
   public removeTeam(teamId: number): void {
-    // TODO
+    this.teamService.remove(teamId).subscribe(() => {
+      this.teamData.refresh();
+    });
   }
 }
