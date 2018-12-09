60158f42cedbdca0980704fcd7f63cd0dadc016f
fi3te <fi3te@users.noreply.github.com>
2018-12-09 16:29:24 +0100

    Scenario config settings component added

--- src/app/build-assignment/scenario-config/scenario-config.component.ts
+++ src/app/build-assignment/scenario-config/scenario-config.component.ts
@@ -2,15 +2,11 @@ import { Component, Input, EventEmitter, Output } from '@angular/core';
 
 import { DTO } from '../../generated-dtos.model';
 import ScenarioConfigDTO = DTO.ScenarioConfigDTO;
-import { Options } from 'ng5-slider';
 
 @Component({
   selector: 'app-scenario-config',
   templateUrl: './scenario-config.component.html',
-  styleUrls: [
-    '../../../scss/local/slider.scss',
-    './scenario-config.component.scss'
-  ]
+  styleUrls: ['./scenario-config.component.scss']
 })
 export class ScenarioConfigComponent {
   @Input()
@@ -18,14 +14,6 @@ export class ScenarioConfigComponent {
 
   @Output()
   public remove = new EventEmitter<ScenarioConfigDTO>();
-
-  public sliderOptions: Options = {
-    floor: 1,
-    ceil: 254,
-    hideLimitLabels: true,
-    hidePointerLabels: true,
-    showSelectionBar: true
-  };
 
   public removeScenarioConfig(): void {
     this.remove.emit(this.scenarioConfig);
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,19 +3,26 @@ import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RenameModalComponent } from './rename-modal/rename-modal.component';
+import { ScenarioConfigSettingsComponent } from './scenario-config-settings/scenario-config-settings.component';
+import { Ng5SliderModule } from 'ng5-slider';
+import { ColorPickerModule } from 'ngx-color-picker';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    ModalBuildingBlockModule
+    ModalBuildingBlockModule,
+    Ng5SliderModule,
+    ColorPickerModule
   ],
   declarations: [
-    RenameModalComponent
+    RenameModalComponent,
+    ScenarioConfigSettingsComponent
   ],
   exports: [
     ModalBuildingBlockModule,
-    RenameModalComponent
+    RenameModalComponent,
+    ScenarioConfigSettingsComponent
   ]
 })
 export class SharedModule { }
