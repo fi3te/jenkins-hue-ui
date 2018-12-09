26c0d18aec9e5caedf2f971b8989bc966796f263
wennier <fiete.wennier@adesso.de>
2018-12-09 19:12:26 +0100

    Color test component implemented

--- src/app/color-test/color-test.module.ts
+++ src/app/color-test/color-test.module.ts
@@ -1,12 +1,18 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ColorTestComponent } from './color-test.component';
 import { ColorTestRoutingModule } from './color-test-routing.module';
+import { SharedModule } from '../shared/shared.module';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
   imports: [
     CommonModule,
-    ColorTestRoutingModule
+    ColorTestRoutingModule,
+    SharedModule,
+    FormsModule,
+    NgSelectModule
   ],
   declarations: [ColorTestComponent]
 })
