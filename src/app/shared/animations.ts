22999194618240185d53f26c6364247c4bd4ff8c
fi3te <fi3te@users.noreply.github.com>
2018-12-28 18:00:47 +0100

    Overflow bug fixed

--- src/app/shared/animations.ts
+++ src/app/shared/animations.ts
@@ -3,7 +3,7 @@ import { state, transition, animate, style, trigger } from '@angular/animations'
 export class Animations {
     public static slideInOut = trigger('slideInOut', [
         state('true', style({ height: '0' })),
-        state('false', style({ height: '*' })),
+        state('false', style({ height: '*', overflow: 'visible' })),
         transition('1 => 0', animate('400ms ease-in')),
         transition('0 => 1', animate('400ms ease-out'))
     ]);
