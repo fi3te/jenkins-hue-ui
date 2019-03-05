ff44f829d9ae1f0877fa5aa3a11ddbcdd4ca5b24
fi3te <fi3te@users.noreply.github.com>
2019-03-05 16:41:48 +0100

    Animation bug fixed

--- src/app/shared/animations.ts
+++ src/app/shared/animations.ts
@@ -1,10 +1,17 @@
 import { state, transition, animate, style, trigger } from '@angular/animations';
 
 export class Animations {
-    public static slideInOut = trigger('slideInOut', [
-        state('true', style({ height: '0' })),
-        state('false', style({ height: '*', overflow: 'visible' })),
-        transition('1 => 0', animate('400ms ease-in')),
-        transition('0 => 1', animate('400ms ease-out'))
-    ]);
+  public static slideInOut = trigger('slideInOut', [
+      state('true', style({ height: '0' })),
+      state('false', style({ height: '*' })),
+      transition('1 => 0', animate('400ms ease-in')),
+      transition('0 => 1', animate('400ms ease-out'))
+  ]);
+
+  public static slideInOutWithOverflow = trigger('slideInOutWithOverflow', [
+    state('true', style({ height: '0' })),
+    state('false', style({ height: '*', overflow: 'visible' })),
+    transition('1 => 0', animate('400ms ease-in')),
+    transition('0 => 1', animate('400ms ease-out'))
+  ]);
 }
